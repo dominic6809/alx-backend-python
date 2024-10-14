20c301fc4d457d73b92e4be8cb94520ed20662f4
dominic6809 <muuomusyoki018@gmail.com>
2024-10-14 07:00:59 +0000

    implement the task_wait_random function using regular function syntax, which returns an asyncio.Task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+module that contains a function that creates an asyncio.Task for wait_random.
+"""
+
+
+import asyncio
+from 0-basic_async_syntax import wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Takes an integer max_delay and returns an asyncio.Task.
+
+    Arguments:
+        max_delay (int): Maximum number of seconds to wait.
+
+    Returns:
+        asyncio.Task: A task that will execute the wait_random coroutine.
+    """
+    return asyncio.create_task(wait_random(max_delay))
